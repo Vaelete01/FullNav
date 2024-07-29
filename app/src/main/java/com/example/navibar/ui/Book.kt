a669b7f8490c6cb752d62f2ad241a62ddb94dc48
Vaelete01 <anna.zdancuka@gmail.com>
2024-07-29 14:19:32 +0300

    Update Book.kt

--- app/src/main/java/com/example/navibar/ui/Book.kt
+++ app/src/main/java/com/example/navibar/ui/Book.kt
@@ -3,7 +3,7 @@ package com.example.navibar.ui
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "books")
+@Entity
 data class Book(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
